ea3fa9646a6c2d63cfa6cac23ea0283cc3f8db83
Guillermo Martinez <dumblemoe@gmail.com>
2019-10-31 15:29:32 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'categories/show'
+  resources :accounts
+  get 'accounts/new'
 
-  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
-  resources :categories
-end
+  get
